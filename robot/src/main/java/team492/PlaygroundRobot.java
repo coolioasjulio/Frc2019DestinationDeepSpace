1b564372d4834197e0241deb94e13f24f0df47f9
Abhay D <a.deshpande012@gmail.com>
2019-08-05 20:11:34 -0700

    Some fixes, switched to spark

--- robot/src/main/java/team492/PlaygroundRobot.java
+++ robot/src/main/java/team492/PlaygroundRobot.java
@@ -1,7 +1,7 @@
 package team492;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import frclib.FrcCANTalon;
+import frclib.FrcCANSparkMax;
 import hallib.HalDashboard;
 import trclib.TrcMotor;
 import trclib.TrcPidController;
@@ -15,8 +15,8 @@ import java.util.Date;
 
 public class PlaygroundRobot extends TimedRobot
 {
-    private TrcTalon talon;
-    private final double maxVel = 1;
+    private TrcSpark spark;
+    private final double maxVel = 5676.0 / 60.0;
     private boolean useTorque = true;
     private double startTime;
     private PrintStream out;
@@ -29,15 +29,16 @@ public class PlaygroundRobot extends TimedRobot
     @Override
     public void robotInit()
     {
-        talon = new TrcTalon("talon", 1);
+        spark = new TrcSpark("spark", 4);
         HalDashboard.putBoolean("useTorque", true);
+        HalDashboard.putNumber("vel", 0);
     }
 
     @Override
     public void autonomousInit()
     {
-        talon.setOdometryEnabled(true);
-        talon.enableVelocityMode(maxVel, new TrcPidController.PidCoefficients(0, 0, 0, 1 / maxVel));
+        spark.setOdometryEnabled(true);
+        spark.enableVelocityMode(maxVel, new TrcPidController.PidCoefficients(0, 0, 0, 1.0));
         startTime = TrcUtil.getCurrentTime();
         useTorque = HalDashboard.getBoolean("useTorque", true);
         try
@@ -56,11 +57,12 @@ public class PlaygroundRobot extends TimedRobot
     @Override
     public void autonomousPeriodic()
     {
-        talon.set(0.5);
+        spark.set(0.5);
         double time = TrcUtil.getCurrentTime() - startTime;
-        double power = talon.getPower();
-        double normalizedVel = talon.getVelocity() / maxVel;
+        double power = spark.getPower();
+        double normalizedVel = spark.getVelocity() / maxVel;
         String line = String.format("%.3f,%.2f,%.2f\n", time, power, normalizedVel);
+        HalDashboard.putNumber("vel", normalizedVel);
         System.out.print(line);
         if (out != null)
         {
@@ -71,9 +73,9 @@ public class PlaygroundRobot extends TimedRobot
     @Override
     public void disabledInit()
     {
-        talon.setOdometryEnabled(false);
-        talon.disableVelocityMode();
-        talon.set(0.0);
+        spark.setOdometryEnabled(false);
+        spark.disableVelocityMode();
+        spark.set(0.0);
         if (out != null)
         {
             out.close();
@@ -83,22 +85,29 @@ public class PlaygroundRobot extends TimedRobot
     @Override
     public void disabledPeriodic()
     {
-        talon.set(0.0);
+        spark.set(0.0);
     }
 
-    private class TrcTalon extends TrcMotor
+    /**
+     * This is just a wrapper that's basically a partial implementation of FrcCANSparkMax. This is because
+     * we want to use the TrcMotor velocity control code. This was initially made for talons, since the talon
+     * implements native velocity control. The spark doesn't, but I didn't want to scrap this work, so whatever.
+     */
+    private class TrcSpark extends TrcMotor
     {
-        public FrcCANTalon talon;
+        public FrcCANSparkMax spark;
 
-        public TrcTalon(String instanceName, int id)
+        public TrcSpark(String instanceName, int id)
         {
             super(instanceName);
-            this.talon = new FrcCANTalon(instanceName, id);
+            this.spark = new FrcCANSparkMax(instanceName, id, true);
+            this.spark.motor.disableVoltageCompensation();
         }
 
         @Override
         protected double transformTorqueToMotorPower(double percentTorque)
         {
+            System.out.println(percentTorque);
             if (useTorque)
             {
                 return super.transformTorqueToMotorPower(percentTorque);
@@ -109,91 +118,91 @@ public class PlaygroundRobot extends TimedRobot
         @Override
         public double getMotorPosition()
         {
-            return talon.getMotorPosition();
+            return spark.getMotorPosition();
         }
 
         @Override
         public double getPosition()
         {
-            return talon.getPosition();
+            return spark.getPosition();
         }
 
         @Override
         public double getVelocity()
         {
-            return talon.getVelocity();
+            return spark.getVelocity();
         }
 
         @Override
         public void setMotorPower(double power)
         {
-            talon.setMotorPower(power);
+            spark.setMotorPower(power);
         }
 
         @Override
         public boolean getInverted()
         {
-            return talon.getInverted();
+            return spark.getInverted();
         }
 
         @Override
         public double getPower()
         {
-            return talon.getPower();
+            return spark.getPower();
         }
 
         @Override
         public boolean isLowerLimitSwitchActive()
         {
-            return talon.isLowerLimitSwitchActive();
+            return spark.isLowerLimitSwitchActive();
         }
 
         @Override
         public boolean isUpperLimitSwitchActive()
         {
-            return talon.isUpperLimitSwitchActive();
+            return spark.isUpperLimitSwitchActive();
         }
 
         @Override
         public void resetPosition(boolean hardware)
         {
-            talon.resetPosition(hardware);
+            spark.resetPosition(hardware);
         }
 
         @Override
         public void setBrakeModeEnabled(boolean enabled)
         {
-            talon.setBrakeModeEnabled(enabled);
+            spark.setBrakeModeEnabled(enabled);
         }
 
         @Override
         public void setInverted(boolean inverted)
         {
-            talon.setInverted(inverted);
+            spark.setInverted(inverted);
         }
 
         @Override
         public void setPositionSensorInverted(boolean inverted)
         {
-            talon.setPositionSensorInverted(inverted);
+            spark.setPositionSensorInverted(inverted);
         }
 
         @Override
         public void setSoftLimitEnabled(boolean lowerLimitEnabled, boolean upperLimitEnabled)
         {
-            talon.setSoftLimitEnabled(lowerLimitEnabled, upperLimitEnabled);
+            spark.setSoftLimitEnabled(lowerLimitEnabled, upperLimitEnabled);
         }
 
         @Override
         public void setSoftLowerLimit(double position)
         {
-            talon.setSoftLowerLimit(position);
+            spark.setSoftLowerLimit(position);
         }
 
         @Override
         public void setSoftUpperLimit(double position)
         {
-            talon.setSoftUpperLimit(position);
+            spark.setSoftUpperLimit(position);
         }
     }
 }
